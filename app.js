aadf53f497fba207556d31db2cfdd437f8903c88
Tanya D <ttdlabaj@gmail.com>
2022-01-07 22:56:36 -0500

    Additional corrections

--- app.js
+++ app.js
@@ -15,41 +15,56 @@ const getBooks = async () => {
         //display books matching query
         const books = (data.items) 
         getFinalList(books)
+        return books
     } catch(err) {
         console.log(err)
     }
 }
 
 // get additional information about books from books array
-const getFinalList = async (books) => {
-    const booklist = await books.map((r, index) =>({
+const getFinalList = (books) => {
+    const booklist = books.map((r, index) => ({
         'id': index,
         'author': r.volumeInfo.authors,        
         'title': r.volumeInfo.title,        
         'publisher': r.volumeInfo.publisher       
     }))
-    console.log(booklist)
+    for (let i = 0; i < booklist.length; i++) {
+        console.log(`\n\nID: ${booklist[i]['id']} \nTitle: ${booklist[i]['title']} \nAuthor: ${booklist[i]['author']} \nPublisher: ${booklist[i]['publisher']}`)
+    }
+
     addBookToList(booklist)    
     return booklist
 }
+let readinglist = []
 // Find book by ID and add book to readingList.txt
-addBookToList = async (booklist) => {
-    let readinglist = []
-    const idNum = prompt("Enter ID of book to add to reading list: ");
+addBookToList = (booklist) => {
+
+    let idNum = prompt("Enter ID of book to add to reading list: ");
+    if (idNum === " " || idNum === "") {
+        console.log('Enter a valid number: It is not in the search results.')
+        idNum = prompt("Enter ID of book to add to reading list: ");
+    }
+
+    if (idNum <= 0 || idNum > booklist[booklist.length - 1]['id']) {
+        console.log('Enter a valid number: It is not in the search results.')
+        idNum = prompt("Enter ID of book to add to reading list: ");
+    }
+
     for (let i=0; i < booklist.length; i++) {
         if (idNum == booklist[i]['id']) {
-            readinglist.push(booklist[i])            
-        }
-        
+            readinglist.push(booklist[i])
+        }     
     }
+
     const cleanedList = `Title: ${readinglist[0].title} \nAuthor: ${readinglist[0].author} \nPublisher: ${readinglist[0].publisher} \n`
 
-    await fs.appendFile('readingList.txt', cleanedList, (err) => {
+    fs.appendFile('readingList.txt', cleanedList, (err) => {
         if (err) {
             console.log(err)
         }
         else {
-            console.log("\nBooks in reading list:\n",
+            console.log("\nBooks in reading list:\n\n",
             fs.readFileSync("readingList.txt", "utf8"));
         }
     })
@@ -57,6 +72,8 @@ addBookToList = async (booklist) => {
 
 console.log(getBooks())
 
-
-
-
+module.exports = {
+    getBooks,
+    getFinalList,
+    addBookToList,
+};
---
+++ app.test.js
@@ -0,0 +1,8 @@
+const getBooks = require('./getBooks');
+const getFinalList = require('./getFinalList');
+const addBookToList = require('./addBookToList');
+
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+});
